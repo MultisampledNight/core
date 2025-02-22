293442022192c8d29faa8966b58cf5bbcd160a88
MultisampledNight <contact@multisamplednight.com>
2025-02-22 15:39:14 +0100

    fix(elusive): unsuccessfully try to improve all-rebuild situation

--- system/elusive/configuration.nix
+++ system/elusive/configuration.nix
@@ -2,28 +2,41 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 with lib;
+with builtins;
 let
   efiArch = pkgs.stdenv.hostPlatform.efiArch;
   system = config.system;
 
   core = ../..;
+  shells = map (sh: path { path = sh; })
+    (map
+      (shell: pkgs.callPackage
+        (core + /lib/nix/shell/${shell}/default.nix)
+        {}
+      )
+      [
+        "python"
+        "rust"
+        "julia"
+      ]);
+
+  channel = mapAttrs (_: storePath) {
+    nixos = <nixos>;
+    nixosUnstable = <nixos-unstable>;
+  };
 
   # set up common things and symlinks for my workflow when actually working
   rootTemplate = pkgs.runCommand
     "root-template"
-    rec {
-      inherit core;
+    (rec {
       configPatch = ./config.patch;
       buildInputs = with pkgs; [ python3 ];
       toolchain = pkgs.stdenv.hostPlatform.config;
 
-      nixos = <nixos>;
-      nixosUnstable = <nixos-unstable>;
-
       rustChannel = "stable";
       rustInstall = ~/.rustup/toolchains/${rustChannel}-${toolchain};
       rustupSettings = ~/.rustup/settings.toml;
-    }
+    })
     ''
       mkdir $out
       user=multisn8
@@ -31,12 +44,12 @@ let
 
       # despite its name (TODO: change that sometime) it can also take care of copying configs, shells, the works
       python \
-        $core/bootstrap/distribute_symlinks.py \
+        ${core}/bootstrap/distribute_symlinks.py \
         --only-user --actually-install \
         --root $out --user $user
 
       mkdir -p $home/zero
-      cp -r $core $home/zero/core
+      cp -r ${core} $home/zero/core
 
       # adjust some things to fit to a VM
       chmod -R 777 $out
@@ -47,11 +60,11 @@ let
       cp -r $rustInstall $home/.rustup/toolchains/$rustChannel-$toolchain;
       cp $rustupSettings $home/.rustup/settings.toml;
 
-      # making nix' channels available so one can use nix-shell
+      # copy host channels
       target=$out/nix/var/nix/profiles/per-user/root/channels
       mkdir -p $target
-      cp -r $nixos $target/nixos
-      cp -r $nixosUnstable $target/nixos-unstable
+      cp -r ${channel.nixos}/ $target/nixos
+      cp -r ${channel.nixosUnstable}/ $target/nixos-unstable
 
       # make everything immutable, even in VM
       chmod -R 550 $out
@@ -95,8 +108,7 @@ in {
       };
       root = {
         contents."/".source = rootTemplate;
-        storePaths = [system.build.toplevel];
-        # TODO: get rid of elusive-ssh and elusive-rsync and do it all via ssh config
+        storePaths = [system.build.toplevel] ++ shells ++ (attrValues channel);
         repartConfig = {
           Type = "root";
           Label = "ROOT";
@@ -132,7 +144,6 @@ in {
     boot = null;
     baremetal = false;
     ssh = true;
-    wayland = true;
   };
 
   users = {
